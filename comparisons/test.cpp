c2beadeda5cb5d36d70eab55829f2886f5337d3e
Michael Esses <messes@umich.edu>
2014-07-04 10:43:33 -0400

    Cleaned up quicksort comparing test file.

--- comparisons/test.cpp
+++ comparisons/test.cpp
@@ -12,15 +12,6 @@ int main()
 	LastPivot lp;
 	MedianPivot mp;
 
-	int size = 10;
-	int arr1[] = { 3, 9, 8, 4, 6, 10, 2, 5, 7, 1 };
-	int arr2[] = { 3, 9, 8, 4, 6, 10, 2, 5, 7, 1 };
-	int arr3[] = { 3, 9, 8, 4, 6, 10, 2, 5, 7, 1 };
-
-	assert(fp.firstPivot(arr1, size) == 25);
-	assert(lp.lastPivot(arr2, size) == 29);
-	assert(mp.medianPivot(arr3, size) == 21);
-
 	const int SIZE = 10000;
 	ifstream in("QuickSort.txt");
 	int arr[SIZE];
